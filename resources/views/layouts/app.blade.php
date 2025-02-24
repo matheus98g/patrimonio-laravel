4581bc20083debcd27478b4adcdf8b1d65e27d19
matheus98g <gollmannmatheus@gmail.com>
2025-02-24 23:41:20 +0000

    added favicon (storage/assets/icons/)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="icon" href="{{ asset('storage/assets/icons/senac-logo-sem-fundo.ico') }}" type="image/x-icon">
+
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
