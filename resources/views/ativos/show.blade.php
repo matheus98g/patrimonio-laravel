0d47ce631f37dcd7e59b4a2bfdc2ccc67c1f6edd
matheus98g <gollmannmatheus@gmail.com>
2025-03-07 01:01:02 +0000

    minor

--- resources/views/ativos/show.blade.php
+++ resources/views/ativos/show.blade.php
@@ -9,12 +9,12 @@
 <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 ">
-                    <div id="imagem-ativo-detalhe">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex">
+                   <div id="imagem-ativo-detalhe">
                         @if ($ativo->imagem)
                             <div class="flex justify-center">
                                 <div
-                                    class="relative w-20 h-20 md:w-24 md:h-24 overflow-hidden rounded-lg shadow-sm hover:shadow-md transition-shadow">
+                                    class="relative w-20 h-20 md:w-32 md:h-32 lg:w-48 lg:h-48 max-w-96 max-h-72 overflow-hidden rounded-lg shadow-sm hover:shadow-md transition-shadow">
                                     <img src="{{ Storage::url($ativo->imagem) }}"
                                         alt="Imagem do ativo {{ $ativo->descricao }}"
                                         class="w-full h-full object-cover hover:scale-105 transition-transform duration-200">
@@ -22,14 +22,29 @@
                             </div>
                         @endif
                     </div>
-                    <div>
-                        <span>Descrição: {{ $ativo->descricao }}</span>
-                    </div>
-                    <div>
-                        <span>Observação: {{ $ativo->observacao }}</span>
-                    </div>
-                    <div>
-                        <span>Quantidade Disponivel: {{ $ativo->quantidade }}</span>
+
+
+                    <div id="info-ativo-detalhe" class="ml-4">
+                        <div>
+                            <strong class="text-xl">{{ $ativo->descricao }}</strong>
+                        </div>
+                        <div>
+                            <span>Observação: {{ $ativo->observacao }}</span>
+                        </div>
+                        <div>
+                            <span>Quantidade Total: {{ $ativo->quanitdade }}</span>
+                        </div>
+                        <div>
+                            <span>Quantidade Mínima: {{ $ativo->quanitdade_min }}</span>
+                        </div>
+                        <div>
+                            @php
+                                $quantidadeDisp = optional($ativosDisp->firstWhere('id_ativo', $ativo->id))->quantidade_disp ?? 0;
+                            @endphp
+                            <span>Quantidade Disponivel:  
+                                {{ $quantidadeDisp }}
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
