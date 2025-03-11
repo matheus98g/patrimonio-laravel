01e4e8a49a1319c3863a5638a41c067cb1771f53
matheus98g <gollmannmatheus@gmail.com>
2025-03-11 22:57:44 +0000

    adicionado um log info no midleware do recaptcha para avisar quando um login com sucesso é realizado e mostra o ip

--- app/Http/Middleware/VerifyRecaptcha.php
+++ app/Http/Middleware/VerifyRecaptcha.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log; // Adicionando o uso do Log
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -45,6 +46,9 @@ class VerifyRecaptcha
                 'recaptcha' => 'Verificação reCAPTCHA falhou.'
             ]);
         }
+
+        // Log de sucesso de verificação do reCAPTCHA
+        Log::info('Login bem-sucedido - reCAPTCHA validado com sucesso para o IP: ' . $request->ip());
 
         return $next($request);
     }
