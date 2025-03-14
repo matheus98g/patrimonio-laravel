d5f70e6ce973d9d571fe10bc7a07ac60c8e77e25
matheus98g <gollmannmatheus@gmail.com>
2025-03-14 01:05:48 +0000

    alterado o app para usar navbar enquanto o sidebar nao esta pronto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.sidebar')
+            @include('layouts.navbar')
 
             <!-- Page Heading -->
             @isset($header)
